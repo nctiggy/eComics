538a13b7c29223914f40d42aa65a1557853ffb76
Craig Smith <nctiggy@gmail.com>
2011-07-13 20:17:26 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EComics::Application.config.secret_token = '9b3ee155998a5ce71abbd95b087d25f0a9beebef9e7e21aebf2f6ad02554b693a3f4ec49c16a5dcdd68cd54123e2ac4b893f0c7cd19e08b0096cd7dc63067bd0'
